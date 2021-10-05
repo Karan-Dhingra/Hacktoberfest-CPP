55b3bbc961f9f3dd6ea2a22ef26c38a90665ce2c
Revanth <82746780+Revanthpalukuri@users.noreply.github.com>
2021-10-05 13:08:22 +0530

    Update Inorder Traversal.cpp

--- Inorder Traversal.cpp
+++ Inorder Traversal.cpp
@@ -1,16 +1,30 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+struct Node {
+    int data;
+    struct Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+void Inorder(Node* root)
+{
+    if(root==NULL) return;
+    Inorder(root->left);
+    cout<<root->data<<" ";
+    Inorder(root->right);
+}
 
 int main()
 {
-   string a;
-   cin>>a;
-   for(int i=0;i<3;i++)
-   { if(a.at(i)=='1')
-       a.at(i)='9';
-       else
-       a.at(i)='1';}
-       cout<<a;
-    return 0;
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    cout<<"The Inorder traversal of binary tree is \n";
+    Inorder(root);
 }
