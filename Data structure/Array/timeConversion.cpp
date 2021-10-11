ff3793d1dedb6b33ddbb7b85b1b43cd66b370654
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2021-10-11 09:40:10 +0530

    Added TimeConversion Problem Solution
    It will convert the time of 12hours in 24 hours

---
+++ Data structure/Array/timeConversion.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+void timeConversion(string str){
+	int h1=(int)str[1]-'0';
+	int h2 = (int)str[0]- '0';
+	int hh = (h2*10 + h1%10);
+	
+	if(str[8]=='A'){
+		if(hh==12){
+			cout<<"00";
+			for(int i=2;i<=7 ; i++){
+				cout<<str[i];
+			}
+		}else{
+			for(int i=0;i<=7;i++){
+				cout<<str[i];
+			}
+		}
+	}
+	
+	else{
+		if(hh==12){
+			cout<<"12";
+			for(int i=2 ; i<=7 ;i++){
+				cout<<str[i];
+			}
+		}else{
+			hh = hh+12;
+			cout<<hh;
+			for(int i=2 ; i<=7 ; i++){
+				cout<<str[i];
+			}
+		}
+	}
+}
+
+int main(){
+	
+	string str=" ";
+	getline(cin,str);
+	timeConversion(str);
+
+
+
+return 0;
+}
+
